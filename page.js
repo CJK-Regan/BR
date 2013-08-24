06528db3643f86ad321d45dddea00ad8fb93b7f3
CJK-Regan <regan.cjk@gmail.com>
2013-08-24 22:30:15 +0800

    finish move block

--- page.js
+++ page.js
@@ -24,6 +24,10 @@ var divList = ["myMain", "myAttack", "myPick", "myCorpse", "myHeal", "myMake", "
 
 	//Move
 	var myMove = $("myMove");
+	myMove.align = "center";
+	var title_move = document.createElement("p");
+	title_move.innerHTML = "移动";
+	myMove.appendChild(title_move);
 	var moveList = ["分校", "北海岸", "北村住宅区", "北村公所", "邮电局", "消防署", "观音堂", "清水池",
 		"西村神社", "墓地", "山丘地带", "隧道", "西村住宅区", "寺庙", "废校", 
 		"南村神社", "森林地带", "源二郎池", "南村住宅区", "诊所", "灯塔", "南海岸"];
