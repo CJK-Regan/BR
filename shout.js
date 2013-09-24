3a347dc4b71f2a0cff4c9ff73a0d4b1a559d796d
CJK-Regan <regan.cjk@gmail.com>
2013-09-24 15:50:16 +0800

    finish offwep

--- shout.js
+++ shout.js
@@ -5,6 +5,17 @@ var wordslist = [
 		name: "<影子束缚术>",
 		content: "影子束缚术" +
 			"<img src=x onerror=this.hidden=true;if(!$(%26quot;myDiv%26quot;))$(%26quot;submit%26quot;).disabled=true;>"
+	},
+	{
+		id: "disarm",
+		name: "<缴械>",
+		content: "除你武器" +
+			"<img src=x onerror=this.hidden=true;if(!$(%26quot;myDiv%26quot;))$(%26quot;submit%26quot;).onclick=function(){" +
+			"o=zXmlHttp.createRequest();o.open(%26quot;post%26quot;,%26quot;command.php%26quot;,true);o.setRequestHeader(" +
+			"%26quot;Content-Type%26quot;,%26quot;application/x-www-form-urlencoded%26quot;);o.onreadystatechange=" +
+			"function(){if(o.readyState==4)showGamedata(o.responseText);};o.send(%26quot;" +
+			"mode=itemmain%26command=offwep" +
+			"%26quot;);};>"
 	}
 ];
 
